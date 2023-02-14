bd63c36117034b35f398b01809f372b8968794ae
MaC <miromiri007@gmail.com>
2023-02-14 16:57:17 +0100

    modificato un po il css, niente di che

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-
-validates :username, presence: true, uniqueness: true
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only:[:new, :create]
+  
   root "articles#index"
 
   resources :articles do
     resources :comments
-
-
-    get '/login', to: 'sessions#new', as: 'login'
-    post '/sessions', to: 'sessions#create', as: 'sessions'
-    delete '/sessions', to: 'sessions#destroy'
+  end
+  
 
     
-  end
 end
