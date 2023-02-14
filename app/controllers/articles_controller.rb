5b042a9b8647c3c564199d13bb89f0222c5bf1d8
MaC <miromiri007@gmail.com>
2023-02-14 10:11:49 +0100

    modifiato alcune ccose, aggiunto css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh" , password: "secret", except: [:index, :show]
+
+
   def index
     @articles = Article.all
   end
@@ -50,7 +54,7 @@ class ArticlesController < ApplicationController
 
   private 
     def article_params
-      params.require(:article).permit(:title, :body, :status)
+      params.require(:article).permit(:title, :body, :author, :status)
     end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,8 @@ class Article < ApplicationRecord
 
     has_many :comments, dependent: :destroy
 
-    
     validates :title, presence: true
+    validates :author, presence:true
     validates :body,  presence: true, length: { minimum: 10 }
+    
 end
